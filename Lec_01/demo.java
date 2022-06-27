34acd351775b7b49d5a5bab97a9901f5e4e8777f
Lakshya-CB <78645656+Lakshya-CB@users.noreply.github.com>
2022-06-27 12:57:22 +0530

    Add files via upload

---
+++ Lec_01/demo.java
@@ -0,0 +1,11 @@
+package Lec_01;
+
+public class demo {
+
+	public static void main(String[]args) {
+
+		System.out.println("kuch print karde bhai");
+		System.out.println("bhaiya ho gya");
+
+	}
+}
