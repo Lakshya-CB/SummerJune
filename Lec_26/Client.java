96a5ffd3b7a5305d44a22f62f868f6d9ac5451f2
Lakshya-CB <78645656+Lakshya-CB@users.noreply.github.com>
2022-08-23 13:12:15 +0530

    Add files via upload

---
+++ Lec_26/Client.java
@@ -0,0 +1,21 @@
+package Lec_26;
+
+public class Client {
+	public static void main(String[] args) {
+		LinkedList LL = new LinkedList();
+		LL.addLast(10);
+		LL.addLast(20);
+		LL.addLast(30);
+		LL.addLast(40);
+		LL.addLast(50);
+		LL.addLast(60);
+		LL.addLast(70);
+		LL.addLast(80);
+		LL.addLast(90);
+
+		LL.addLast(100);
+		LL.addLast(110);
+		LL.kRev(3);
+		LL.disp();
+	}
+}
